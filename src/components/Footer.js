8b0692f8021698055fd05dfecc8aa3e2b4b92a54
bodealarif-cell <bodealarif@gmail.com>
2026-04-27 20:12:12 +0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="text-center py-6 mt-10 border-t border-gray-700">
+      <p className="text-gray-400">تصميم بقلم أبو العارف جزاه الله خيراً</p>
+    </footer>
+  );
+}
+
+export default Footer;
